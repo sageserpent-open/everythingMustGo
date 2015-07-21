b9a2f08b3485ed1313cc44dac465b54150d9a06d
Gerard <Gerard@.(none)>
2015-07-21 22:01:39 +0100

    A new failing test after much reading about Scalacheck in Scalatest. More thoughts about the tests....

--- build.sbt
+++ build.sbt
@@ -2,4 +2,6 @@ scalaVersion := "2.11.6"
 
 libraryDependencies += "org.scalatest" % "scalatest_2.11" % "2.2.4" % "test"
 
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.12.4" % "test"
+
 name := "everyThingMustGo"
--- src/test/scala/com/sageserpent/everythingMustGo/CheckoutTests.scala
+++ src/test/scala/com/sageserpent/everythingMustGo/CheckoutTests.scala
@@ -1,14 +1,31 @@
 package com.sageserpent.everythingMustGo
 
 import org.scalatest.FlatSpec
+import org.scalatest.prop.Checkers
+import org.scalacheck.Prop
+import org.scalacheck.Gen
 
-class CheckoutTests extends FlatSpec {
-"No items" should "result in no billing" in {
-  assert(0 === Checkout(Map.empty, Iterable.empty))
-}
+class CheckoutTests extends FlatSpec with Checkers {
+  "No items" should "result in no billing" in {
+    assert(0 === Checkout(Map.empty, Iterable.empty))
+  }
   "One item" should "result in a bill that is its price." in {
     val item = "Alpha"
     val price = 10
     assert(price === Checkout.apply(Map(item -> price), Seq(item)))
+  }
+
+  "N+1 items" should "result in the same bill as purchasing the first N and the last one separately and taking the total." in {
+    val itemPrices = Map[String, Double]("Fred" -> 20, "Frieda" -> 30.5)
+    val itemGenerator = Gen.oneOf(itemPrices.keys.toSeq)
+    val nItemsGenerator = Gen.containerOf[Seq, String](itemGenerator)
+    val testCaseGenerator = for {nItems <- nItemsGenerator
+                                 item <- itemGenerator} yield nItems -> item
+    check(Prop.forAll(testCaseGenerator){case (nItems, item) => {
+      val allInOneGoBill = Checkout.apply(itemPrices, nItems :+ item)
+      val nItemsBill = Checkout.apply(itemPrices, nItems)
+      val itemBill = Checkout.apply(itemPrices, Seq(item))
+      allInOneGoBill === nItemsBill + itemBill
+    }})
   }
 }
