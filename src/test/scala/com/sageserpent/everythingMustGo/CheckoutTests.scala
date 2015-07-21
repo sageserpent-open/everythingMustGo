68ac6943f01d3512ab24c29dcfa36ef1949c752d
Gerard <Gerard@.(none)>
2015-07-21 23:54:26 +0100

    Added new failing test.

--- src/test/scala/com/sageserpent/everythingMustGo/CheckoutTests.scala
+++ src/test/scala/com/sageserpent/everythingMustGo/CheckoutTests.scala
@@ -23,12 +23,13 @@ class CheckoutTests extends FlatSpec with Checkers {
     val nItemsGenerator = Gen.containerOf[Seq, String](itemGenerator)
     val testCaseGenerator = for {nItems <- nItemsGenerator
                                  item <- itemGenerator} yield nItems -> item
-    check(Prop.forAll(testCaseGenerator){case (nItems, item) => {
+    check(Prop.forAll(testCaseGenerator) { case (nItems, item) => {
       val allInOneGoBill = Checkout.apply(itemDatums, nItems :+ item)
       val nItemsBill = Checkout.apply(itemDatums, nItems)
       val itemBill = Checkout.apply(itemDatums, Seq(item))
       allInOneGoBill === nItemsBill + itemBill
-    }})
+    }
+    })
   }
 
   "The order of the items" should "not make any difference to the bill." in {
@@ -38,11 +39,12 @@ class CheckoutTests extends FlatSpec with Checkers {
     val testCaseGenerator = for {items <- itemsGenerator
                                  seed <- seedGenerator
                                  permutation = new Random(seed).shuffle(_: Seq[String])} yield items -> permutation
-    check(Prop.forAll(testCaseGenerator){case (items, permutation) => {
+    check(Prop.forAll(testCaseGenerator) { case (items, permutation) => {
       val billForItemsOneWay = Checkout.apply(itemDatums, items)
       val billForItemsAnotherWay = Checkout.apply(itemDatums, permutation(items))
       billForItemsAnotherWay === billForItemsOneWay
-    } })
+    }
+    })
   }
 
   "A bill" should "not be negative." in {
@@ -53,7 +55,24 @@ class CheckoutTests extends FlatSpec with Checkers {
 
   "An acceptance test" should "be honoured in the observance and not the breach" in {
     2.05 === Checkout.apply(Checkout.productionItemDatums, List("Apple", "Apple", "Orange", "Orange"))
+    2.05 === Checkout.apply(Checkout.productionItemDatums, List("Apple", "Apple", "Orange", "Orange"))
   }
 
+  "Given a bunch of things with a bill, adding a discounted amount of some stuff" should "increase the price by the discount" in {
+    val itemDatumsWithDiscounts = Map("Fred" -> ItemData(price = 20, discount = 2 -> 1), "Frieda" -> ItemData(price = 30.5, discount = 3 -> 2))
 
+    val itemGenerator = Gen.oneOf(itemDatumsWithDiscounts.keys.toSeq)
+    val nItemsGenerator = Gen.containerOf[Seq, String](itemGenerator)
+    val testCaseGenerator = for {nItems <- nItemsGenerator
+                                 item <- itemGenerator} yield nItems -> item
+    check(Prop.forAll(testCaseGenerator) { case (nItems, item) => {
+      val basicBill = Checkout.apply(itemDatumsWithDiscounts, nItems)
+      val ItemData(price, (amountEligibleForDiscount, asIfAmount)) = itemDatumsWithDiscounts(item) // Where are the named pattern variables, like in F#? Sigh.
+      val extraStuff = Seq.fill(amountEligibleForDiscount)(item)
+      val expectedDiscountedPriceIncrease = asIfAmount * Checkout.apply(itemDatumsWithDiscounts, Seq(item))
+      val totalBill = Checkout.apply(itemDatumsWithDiscounts, nItems ++ extraStuff)
+      totalBill === basicBill + expectedDiscountedPriceIncrease
+    }
+    })
+  }
 }
