d3b17b7f6ecfe061c47296d0b2ca6a8a12b58056
Gerard <Gerard@.(none)>
2015-07-21 23:34:32 +0100

    Renames for clarity.

--- src/main/scala/com/sageserpent/everythingMustGo/Checkout.scala
+++ src/main/scala/com/sageserpent/everythingMustGo/Checkout.scala
@@ -1,7 +1,7 @@
 package com.sageserpent.everythingMustGo
 
 object Checkout {
-  def apply(itemPrices: Map[String, ItemData], items: Iterable[String]): Double = items map (itemPrices(_).price) sum
+  def apply(itemDatums: Map[String, ItemData], items: Iterable[String]): Double = items map (itemDatums(_).price) sum
 
-  val productionItemMap = Map("Apple" -> ItemData(price = 0.6), "Orange" -> ItemData(price = 2.05))
+  val productionItemDatums = Map("Apple" -> ItemData(price = 0.6), "Orange" -> ItemData(price = 2.05))
 }
--- src/test/scala/com/sageserpent/everythingMustGo/CheckoutTests.scala
+++ src/test/scala/com/sageserpent/everythingMustGo/CheckoutTests.scala
@@ -16,43 +16,43 @@ class CheckoutTests extends FlatSpec with Checkers {
     assert(price === Checkout.apply(Map(item -> ItemData(price = price)), Seq(item)))
   }
 
-  val itemPrices = Map("Fred" -> ItemData(price = 20), "Frieda" -> ItemData(price = 30.5))
+  val itemDatums = Map("Fred" -> ItemData(price = 20), "Frieda" -> ItemData(price = 30.5))
 
   "N+1 items" should "result in the same bill as purchasing the first N and the last one separately and taking the total." in {
-    val itemGenerator = Gen.oneOf(itemPrices.keys.toSeq)
+    val itemGenerator = Gen.oneOf(itemDatums.keys.toSeq)
     val nItemsGenerator = Gen.containerOf[Seq, String](itemGenerator)
     val testCaseGenerator = for {nItems <- nItemsGenerator
                                  item <- itemGenerator} yield nItems -> item
     check(Prop.forAll(testCaseGenerator){case (nItems, item) => {
-      val allInOneGoBill = Checkout.apply(itemPrices, nItems :+ item)
-      val nItemsBill = Checkout.apply(itemPrices, nItems)
-      val itemBill = Checkout.apply(itemPrices, Seq(item))
+      val allInOneGoBill = Checkout.apply(itemDatums, nItems :+ item)
+      val nItemsBill = Checkout.apply(itemDatums, nItems)
+      val itemBill = Checkout.apply(itemDatums, Seq(item))
       allInOneGoBill === nItemsBill + itemBill
     }})
   }
 
   "The order of the items" should "not make any difference to the bill." in {
-    val itemGenerator = Gen.oneOf(itemPrices.keys.toSeq)
+    val itemGenerator = Gen.oneOf(itemDatums.keys.toSeq)
     val itemsGenerator = Gen.containerOf[Seq, String](itemGenerator)
     val seedGenerator = Gen.oneOf(1, 2)
     val testCaseGenerator = for {items <- itemsGenerator
                                  seed <- seedGenerator
                                  permutation = new Random(seed).shuffle(_: Seq[String])} yield items -> permutation
     check(Prop.forAll(testCaseGenerator){case (items, permutation) => {
-      val billForItemsOneWay = Checkout.apply(itemPrices, items)
-      val billForItemsAnotherWay = Checkout.apply(itemPrices, permutation(items))
+      val billForItemsOneWay = Checkout.apply(itemDatums, items)
+      val billForItemsAnotherWay = Checkout.apply(itemDatums, permutation(items))
       billForItemsAnotherWay === billForItemsOneWay
     } })
   }
 
   "A bill" should "not be negative." in {
-    val itemGenerator = Gen.oneOf(itemPrices.keys.toSeq)
+    val itemGenerator = Gen.oneOf(itemDatums.keys.toSeq)
     val testCaseGenerator = Gen.containerOf[Seq, String](itemGenerator)
-    check(Prop.forAll(testCaseGenerator)(items => 0 <= Checkout.apply(itemPrices, items)))
+    check(Prop.forAll(testCaseGenerator)(items => 0 <= Checkout.apply(itemDatums, items)))
   }
 
   "An acceptance test" should "be honoured in the observance and not the breach" in {
-    2.05 === Checkout.apply(Checkout.productionItemMap, List("Apple", "Apple", "Orange", "Orange"))
+    2.05 === Checkout.apply(Checkout.productionItemDatums, List("Apple", "Apple", "Orange", "Orange"))
   }
 
 
