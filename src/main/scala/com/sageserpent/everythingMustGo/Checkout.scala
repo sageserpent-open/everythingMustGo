29e3c95ce8729b31b410cc7a8d2870c86145c143
Gerard <Gerard@.(none)>
2015-07-24 07:30:00 +0100

    Added a precondition to make explicit that all billable items have to be present in the item datums map as keys.

--- src/main/scala/com/sageserpent/everythingMustGo/Checkout.scala
+++ src/main/scala/com/sageserpent/everythingMustGo/Checkout.scala
@@ -1,7 +1,10 @@
 package com.sageserpent.everythingMustGo
 
 object Checkout {
-  def apply(itemDatums: Map[String, ItemData], items: Iterable[String]): Double = items map (itemDatums(_).price) sum
+  def apply(itemDatums: Map[String, ItemData], items: Iterable[String]): Double = {
+    require(items.forall(itemDatums.contains(_)))
+    items map (itemDatums(_).price) sum
+  }
 
   val productionItemDatums = Map("Apple" -> ItemData(price = 0.6), "Orange" -> ItemData(price = 2.05))
 }
