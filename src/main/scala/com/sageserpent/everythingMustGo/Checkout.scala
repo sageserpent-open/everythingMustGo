3ea93c3c7c613d980bd9cc22bc14cc97e2ba63a3
Gerard <Gerard@.(none)>
2015-07-26 19:13:39 +0100

    This doesn't pass the test, but looks like it should! The clue is in the assumption that multiplication and division should be associative.

--- src/main/scala/com/sageserpent/everythingMustGo/Checkout.scala
+++ src/main/scala/com/sageserpent/everythingMustGo/Checkout.scala
@@ -3,7 +3,12 @@ package com.sageserpent.everythingMustGo
 object Checkout {
   def apply(itemDatums: Map[String, ItemData])(items: Iterable[String]): Double = {
     require(items.forall(itemDatums.contains(_)))
-    items map (itemDatums(_).price) sum
+    val itemLots = items.groupBy(identity).mapValues(_.size)
+    itemLots map {case (item, lot) => {
+      val ItemData(price, (amountEligibleForDiscount, asIfAmount)) = itemDatums(item)
+      val effectiveAmount = asIfAmount * lot / amountEligibleForDiscount + lot % amountEligibleForDiscount
+      effectiveAmount * price
+    }} sum
   }
 
   val productionItemDatums = Map("Apple" -> ItemData(price = 0.6), "Orange" -> ItemData(price = 2.05))
